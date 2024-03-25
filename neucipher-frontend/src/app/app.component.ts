81c591b052476208b392547d0c9da4cb65ccb967
Aradhana Ramamoorthy <159184994+AradhanaRamamoorthy@users.noreply.github.com>
2024-03-25 17:36:29 -0400

    Update app.component.ts

--- neucipher-frontend/src/app/app.component.ts
+++ neucipher-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,13 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  showHomeNavOptions: boolean = false;
+  showBackground: boolean = false;
   title = 'neucipher-frontend';
 
-  toggleNavbarMenu() {
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showHomeNavOptions = (event.url === '/' || event.url === '/home');
+        this.showBackground = this.showHomeNavOptions;
+      }
+    });
+  }
+}
+
+
+  /*toggleNavbarMenu() {
     const menu = document.getElementById('navbarMenu');
     if (menu !== null) {
       menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
     }
   }
 
-}
+}*/
