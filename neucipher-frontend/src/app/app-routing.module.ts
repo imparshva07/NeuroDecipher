2bf65be3b9ef6054a0993b9cb86eebe106f90b77
Aradhana Ramamoorthy <159184994+AradhanaRamamoorthy@users.noreply.github.com>
2024-03-24 20:36:33 -0400

    Update app-routing.module.ts

--- neucipher-frontend/src/app/app-routing.module.ts
+++ neucipher-frontend/src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { LoginDoctorComponent } from './login-doctor/login-doctor.component';
+import { SignupDoctorComponent } from './signup-doctor/signup-doctor.component';
 
 const routes: Routes = [
   //{ path: '', redirectTo: '/signup', pathMatch: 'full' },
   { path: '', component : HomeComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'logindoctor', component: LoginDoctorComponent},
+  { path: 'signupdoctor', component: SignupDoctorComponent}
 ];
 
 @NgModule({
