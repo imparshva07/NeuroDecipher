dd083317af13e678f4f8267499f2f64156a16350
Aradhana Ramamoorthy <159184994+AradhanaRamamoorthy@users.noreply.github.com>
2024-04-06 19:45:46 -0400

    Update index.js

--- backend/data/index.js
+++ backend/data/index.js
@@ -1,4 +1,6 @@
 // This file should import both data files and export them as shown in the lecture code
 import patientDataFunctions from './patients.js';
+import doctorDataFunctions from './doctor.js';
 
 export const patientData = patientDataFunctions;
+export const doctorData =  doctorDataFunctions;
