50823f5b241cca435d89291807737a12e4c86f3c
Aradhana Ramamoorthy <159184994+AradhanaRamamoorthy@users.noreply.github.com>
2024-04-06 19:46:17 -0400

    Doctor Backend Route

---
+++ backend/data/doctor.js
@@ -0,0 +1,46 @@
+// TODO: Export and implement the following functions in ES6 format
+
+import {doctors} from '../config/mongoCollections.js';
+import {ObjectId} from 'mongodb';
+
+const create = async (Userdata
+  ) => {
+    let newUserToBeAdded = {
+      email : Userdata.email,
+      name : Userdata.name,
+      password : Userdata.password,
+      gender : Userdata.gender,
+      contactNumber : Userdata.contactNumber,
+      specialty: Userdata.specialty,
+      ml: Userdata.ml
+    };
+  
+    const doctorsCollection = await doctors();
+    const insertInfo = await doctorsCollection.insertOne(newUserToBeAdded);
+    if (!insertInfo.acknowledged || !insertInfo.insertedId) throw ('Error : doctors not be added');
+    const newId = insertInfo.insertedId.toString();
+    const doctorAdded = await get(newId);
+    return doctorAdded;
+  };
+
+  const findEmail = async (email) => {
+    try {
+        const doctorsCollection = await doctors();
+        const doctor = await doctorsCollection.findOne({ email });
+        return doctor;
+    } catch (error) {
+        throw new Error('Error fetching doctor by email: ' + error.message);
+    }
+};
+
+  const get = async (id) => {
+    const doctorsCollection = await doctors();
+    const doctor = await doctorsCollection.findOne({_id: new ObjectId(id)});
+    if (doctor=== null) throw ('Error : doctor not found');
+    doctor._id = doctor._id.toString();
+    return doctor;
+  };
+
+  const exportMethods = {create, findEmail, get}
+
+  export default exportMethods;
