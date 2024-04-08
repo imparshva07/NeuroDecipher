6505c26aeb97579dc2f1b96ab03aab07ec86cc93
Aradhana Ramamoorthy <159184994+AradhanaRamamoorthy@users.noreply.github.com>
2024-04-07 20:34:08 -0400

    Update signup-doctor.component.ts

--- neucipher-frontend/src/app/signup-doctor/signup-doctor.component.ts
+++ neucipher-frontend/src/app/signup-doctor/signup-doctor.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup-doctor',
@@ -19,10 +20,11 @@ export class SignupDoctorComponent {
   };
   confirmPassword: string = '';
 
-constructor(private authService: AuthService) { }
-
-onSignup() {
-  /*My code*/ console.log('SignupComponent: onSignup method called'); // Log signup form submission
-  this.authService.signup(this.userData); //facing issue in API call
+constructor(private authService: AuthService,private router: Router) { }
+  
+onSignupDoctor() {
+  this.authService.signupDoctor(this.userData); 
+  alert('Signup successful!');
+  this.router.navigate(['/logindoctor']);
 }
 }
