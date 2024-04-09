9e6680a8aeddf48a35df996cafdccb4b550ea683
JinalShah31 <jinalsanghrajka@gmail.com>
2024-04-09 17:48:02 -0400

    Refactored Patient signup

--- neucipher-frontend/src/app/signup/signup.component.ts
+++ neucipher-frontend/src/app/signup/signup.component.ts
@@ -21,7 +21,17 @@ export class SignupComponent {
 
   constructor(private authService: AuthService, private router: Router) { }
 
+  validateContactNumber(): boolean {
+    const contactNumberRegex = /^\d{10}$/; // Regular expression to match exactly 10 digits
+    return contactNumberRegex.test(this.userData.contactNumber);
+  }
+
     onSignup() {
+      if (!this.validateContactNumber()) {
+        // Handle invalid contact number
+        console.log("Invalid contact number. Please enter exactly 10 digits.");
+        return;
+      }
       this.authService.signup(this.userData); //facing issue in API call
       alert('Signup successful!');
       this.router.navigate(['/login']);
